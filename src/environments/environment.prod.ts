927494aa519f95301f15e9c2db447fbb9648090d
tanaka <tanakakenne2000@gmail.com>
2021-07-13 19:27:47 +0200

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  production: true,
+  apiKey: "AIzaSyBUOVKYWNjeAg70ROSCUctvqrlk5AkaabQ",
+  authDomain: "oshop-76f68.firebaseapp.com",
+  projectId: "oshop-76f68",
+  storageBucket: "oshop-76f68.appspot.com",
+  messagingSenderId: "496696599768",
+  appId: "1:496696599768:web:1d9d23f185d8d59a41a3ad",
+  measurementId: "G-R0ZWZSN3GG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBUOVKYWNjeAg70ROSCUctvqrlk5AkaabQ",
+    authDomain: "oshop-76f68.firebaseapp.com",
+    projectId: "oshop-76f68",
+    storageBucket: "oshop-76f68.appspot.com",
+    messagingSenderId: "496696599768",
+    appId: "1:496696599768:web:1d9d23f185d8d59a41a3ad",
+    measurementId: "G-R0ZWZSN3GG"
+  }
 };
 
 /*
